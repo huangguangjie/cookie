db761134c04d3827669351c60bbd336c3beabcb5
huangguangjie <huangguangjie441@pingan.com.cn>
2017-07-10 18:19:41 +0800

    init

---
+++ src/index.es6
@@ -0,0 +1,5 @@
+import 'babel-polyfill'
+import assignDeep from 'assign-deep'
+class Cookie {
+
+}
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,11 +18,7 @@ export default {
         extensions: [' ','.js', '.es6']
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            comments: false,
-            sourceMap: true
-        }),
+        // new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.CommonsChunkPlugin({ name: ['umd'], filename: '[name].js'})
     ],
     devtool: 'source-map'
